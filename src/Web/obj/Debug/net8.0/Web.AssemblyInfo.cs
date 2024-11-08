602f4dd71c71b30771540f209e9b723f4841bccc
CoreDX1 <chrismaquis@gmail.com>
2024-11-08 18:54:14 +0000

    Cambiendo la resputa de Add

--- src/Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
+++ src/Web/obj/Debug/net8.0/Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+06acb1fedd147e2ff00832857c8aa9228aab50dd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a0ca263d0ffe8195d69934a4589db475858f1c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
