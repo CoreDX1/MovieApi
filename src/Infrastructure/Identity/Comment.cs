e3f8f549a5933dddc265d2fd7b021df28e117786
CoreDX1 <chrismaquis@gmail.com>
2024-11-25 11:22:10 -0300

    Actualizando las entidades

---
+++ src/Infrastructure/Identity/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Infrastructure.Identity;
+
+public partial class Comment
+{
+    public int Id { get; set; }
+
+    public int MovieId { get; set; }
+
+    public string Text { get; set; }
+
+    public DateOnly Date { get; set; }
+
+    public int? UsuarioId { get; set; }
+
+    public virtual Movie Movie { get; set; }
+}
