7d2b96028b01c47be0192012297ce243a071436e
CoreDX1 <chrismaquis@gmail.com>
2024-11-26 14:32:13 -0300

    Vamos a crear una rama

--- src/Infrastructure/Data/Repositories/Repository.cs
+++ src/Infrastructure/Data/Repositories/Repository.cs
@@ -2,18 +2,22 @@ using Application.Interfaces.Repositories;
 
 namespace Infrastructure.Data.Repositories;
 
-
-public class Repository<T> : IRepository<T> where T : class
+public class Repository<T> : IRepository<T>
+    where T : class
 {
-    public IReadRepository<T> Read { get; set;}
-    public IWriteRepository<T> Write { get; set;}
+    public IReadRepository<T> Read { get; set; }
+    public IWriteRepository<T> Write { get; set; }
 
-    internal ApiMovieContext DbContext { get;}
+    internal ApiMovieContext DbContext { get; }
 
-    public Repository(ApiMovieContext dbContext, IReadRepository<T> readRepository, IWriteRepository<T> writeRepository)
+    public Repository(
+        ApiMovieContext dbContext,
+        IReadRepository<T> readRepository,
+        IWriteRepository<T> writeRepository
+    )
     {
         DbContext = dbContext;
-         Read = readRepository;
-         Write = writeRepository;
+        Read = readRepository;
+        Write = writeRepository;
     }
 }
